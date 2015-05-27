593d7574f4a24e0dbadf3e840225ad7edd1ed7d6
MrFranky <SuperGrobie88@web.de>
2015-05-27 11:28:06 +0200

    add

---
+++ DayZ_Overpoch_instance_26_Tavi.bat
@@ -0,0 +1,2 @@
+@echo off
+start "arma2" /min "arma2oaserver.exe" -port=2302 "-config=Chaotentrupp_Overpochins\config.cfg" "-cfg=Chaotentrupp_Overpochins\basic.cfg" "-profiles=Chaotentrupp_Overpochins" -name=Chaotentrupp_Overpochins "-mod=@Origins;@DayzOverwatch;@DayZ_Epoch;@DayZ_Overpochins_Server;"
